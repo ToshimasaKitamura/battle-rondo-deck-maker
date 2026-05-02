f51b0fa9a2fec1ee296ea8ae17e41f9ff75a0105
ToshimasaKitamura <toshimasa.witchcraft@gmail.com>
2026-05-02 18:18:26 +0900

    Add base path for GitHub Pages

--- deck-maker/vite.config.js
+++ deck-maker/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/battle-rondo-deck-maker/',
   server: {
     host: true, // ネットワークからのアクセスを許可
   },
